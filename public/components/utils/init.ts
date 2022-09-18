572fd5d692c0441d22f1a802d9eac2ef0c38ca5d
brandon <gwonzhang@gmail.com>
2022-09-18 20:08:43 +0800

    delete public/components/utils/init.ts file

--- public/components/utils/init.ts
+++
@@ -1,7 +0,0 @@
-export default function ensure(component, fun) {
-  if (!fun.includes('(')) {
-    return `window['${component.localName}'].${fun}()`;
-  } else {
-    return `window['${component.localName}'].${fun}`;
-  }
-}
