ae875c46e95af3b5651776592ba361e1485be6b0
brandon <gwonzhang@gmail.com>
2023-08-01 08:29:07 +0800

    Add denoServer.js and denoPDF.js

---
+++ controllers/denoPDF.js
@@ -0,0 +1,84 @@
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+import path from 'node:path';
+import { fileURLToPath } from "node:url";
+import { randomUUID } from "node:crypto";
+import ProblemDetails from "../utils/problemDetails.js";
+import { invoiceLayoutWithoutHeader, invoiceLayoutWithHeader} from "../templates/invoiceLayout.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * @param {import('http').IncomingMessage} req - The request object
+ * @param {import('http').ServerResponse} res - The response object
+ * @param {string} body
+ */
+export const previewPDF = async (req, res, body) => {
+  try {
+    const invoiceHTMLString = invoiceLayoutWithoutHeader(JSON.parse(body));
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    return res.end(invoiceHTMLString);
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(new ProblemDetails({
+      type: 'about:blank',
+      title: "Internal server error",
+      status: 500,
+      detail: `Internal server error while previewing invoice: ${req.url}`,
+      instance: req.url || '',
+    })));
+  }
+}
+
+/**
+ * @param {import('http').IncomingMessage} req - The request object
+ * @param {import('http').ServerResponse} res - The response object
+ * @param {string} body
+ */
+export const createPDF = async (req, res, body) => {
+  try {
+    const template = invoiceLayoutWithHeader(JSON.parse(body));
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      // `headless: true` (default) enables old Headless;
+      // `headless: 'new'` enables new Headless;
+      // `headless: false` enables “headful” mode.
+    });
+    const page = await browser.newPage();
+    await page.setContent(template);
+    await page.emulateMediaType('screen');
+    const uuid = randomUUID();
+    await page.pdf({
+      path: path.join(__dirname, `../static/invoices/${uuid}.pdf`),
+      format: 'A4',
+      scale: 0.9,
+      printBackground: true
+    });
+
+    console.log('done');
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    // JSON.stringify is important here
+    // it will throw error if not present even though pdf will be created
+    res.end(JSON.stringify({
+      // we're already a const frontendFolder = 'static' in server file
+      // invoicePath: `/static/invoices/${uuid}.pdf`
+      invoicePath: `/invoices/${uuid}.pdf`
+    }));
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    return res.end(JSON.stringify(new ProblemDetails({
+      type: 'about:blank',
+      title: "Internal server error",
+      status: 500,
+      detail: `Internal server error while creating invoice: ${req.url}`,
+      instance: req.url || '',
+    })));
+  }
+}
---
+++ denoServer.js
@@ -0,0 +1,91 @@
+// @ts-check
+import http from "node:http";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import ProblemDetails from "./utils/problemDetails.js";
+import { previewPDF, createPDF } from "./controllers/denoPDF.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendFolder = 'static';
+
+/**
+ * @param {http.IncomingMessage} req - The request object
+ * @param {http.ServerResponse} res - The response object
+ */
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, frontendFolder, req.url);
+
+  if (req.url === "/") {
+    filePath = path.join(__dirname, frontendFolder, 'index.html');
+  }
+
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      switch(req.url) {
+        case '/api/pdf/preview':
+          return previewPDF(req, res, body);
+        case '/api/pdf/create':
+          return createPDF(req, res, body);
+      }
+    });
+  } else {
+    // Check if file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 404;
+        return res.end(JSON.stringify(new ProblemDetails({
+          type: 'about:blank',
+          title: "File not found",
+          status: 404,
+          detail: `${req.url} file does not exist on server`,
+          instance: req.url || '',
+        })));
+      }
+
+      // Read the file and send it as the response
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.statusCode = 500;
+          // The "chunk" argument must be of type string or an instance of Buffer or Uint8Array. 
+          // Received an instance of ProblemDetails
+          return res.end(JSON.stringify(new ProblemDetails({
+            type: 'about:blank',
+            title: "Internal server error",
+            status: 500,
+            detail: `Internal server error while fetching ${req.url}`,
+            instance: req.url || '',
+          })));
+        }
+
+        // Set the appropriate content type based on the file extension
+        const extname = path.extname(filePath);
+        let contentType = 'text/plain';
+        if (extname === '.html') {
+          contentType = 'text/html';
+        } else if (extname === '.css') {
+          contentType = 'text/css';
+        } else if (extname === '.js') {
+          contentType = 'text/javascript';
+        } else if (extname === '.pdf') {
+          contentType = 'application/pdf';
+        }
+
+        // Set the content type header and send the data
+        res.setHeader('Content-Type', contentType);
+        res.end(data);
+      });
+    });
+  }
+});
+
+const port = 3003;
+server.listen(port, () => {
+  console.log(`Node server running on port ${port}`);
+});
